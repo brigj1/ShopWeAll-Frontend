b83cad0584ba3668a5c474cd75c54fd8d1a8f719
Brigham <brig.johnson@gmail.com>
2022-06-22 16:51:48 -0400

    start adding components to App, map out component usage in statemap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 
 function App() {
+  const [names, setNames] = useState([]);
+  const [shopDate, setShopDate] = useState([]);
+  const storeName = 'Costco'
+
+  useEffect(() => {
+    fetch("http://localhost:9292/names")
+    .then((res) => res.json())
+    .then(setNames);
+  }, []);
+  const shopper_name = names.find(name_obj => name_obj.name === 'Billy')
+  console.log('shopper_name', shopper_name)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Router>
+        <nav>
+          <ul className="linkClass">
+            <li><Link to='/'>Home</Link></li>
+            {/* <li><Link to='name management'>Name Management</Link></li> */}
+            {/* <li><Link to='shopdates'>Shop Dates</Link></li> */}
+          </ul>
+        </nav>
+        <Routes>
+            <Route path='/' element={ <Home shopper_name={shopper_name} />} />
+          </Routes>
+      </Router>
+  </div>
+);
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+
+
+function Home(shopper_name) {
+    console.log('sn', shopper_name)
+    return (<h1>
+        {shopper_name}
+    </h1>)
+}
+
+export default Home
