441ce76f630788faa90a6c09345ce3d10e2012d8
Angelina <angelinarwhite@gmail.com>
2022-06-22 17:46:42 -0400

    add shopper header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,28 @@
+function Home( { names, orders } ) {
 
+    const shopperName = names.map(name_obj => 
+    {
+        if (name_obj.name === 'Billy')
+        {
+            return name_obj.name
+        }
+    })
 
-function Home(shopper_name) {
-    console.log('sn', shopper_name)
-    return (<h1>
-        {shopper_name}
-    </h1>)
+    const shopDate = orders.map(date_obj => 
+        {
+            if (date_obj.shop_date === '2022-06-25')
+            {
+                return date_obj.shop_date
+            }
+        })
+
+    return (
+        <div>
+            <h2>{ shopperName[0]}</h2>
+            <h2>{ shopDate[0] }</h2>
+            <h2>Store: Costco</h2>
+        </div>
+    )
 }
 
 export default Home
